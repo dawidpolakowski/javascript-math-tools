cb99a1a9ae78bc93b25942f522c5341bf1795c4f
Dawid Polakowski <dawidpolakowski@gmail.com>
2023-11-24 20:45:04 +0000

    refactoring and updates in index

--- multiplication/multiplicationcolour.js
+++ multiplication/multiplicationcolour.js
@@ -5,20 +5,17 @@ function multiplicationMatrixColour() {
     let mArray = new Array(counter)
     let cArray = new Array(counter)
 
-
     let n = 1
 
     for (let i = 1; i < 11; i++) {
         for (let j = 1; j < 11; j++) {
             mArray[n] = i * j
-            
+
             n++
             console.log("n: " + n)
             console.log("i: " + i)
             console.log("j: " + j)
-            console.log("mArray: " + mArray[n-1]);
-
-
+            console.log("mArray: " + mArray[n - 1]);
         }
     }
 
@@ -28,35 +25,23 @@ function multiplicationMatrixColour() {
         if (mArray[i] == 100) document.write('[_' + mArray[i] + '_] ')
         if (mArray[i] > 100) document.write('[_' + mArray[i] + '_] ')
         if ((i % 10) == 0 && i > 0) { document.write('<br>') }
-        if ((i == 100) ) { document.write('<br>') }
+        if ((i == 100)) { document.write('<br>') }
     }
-
-    // for (let i = 1; i < mArray.length; i++) {
-    //     if (mArray[i] < 10) document.write( mArray[i] + '  , ')
-    //     if (mArray[i] >= 10 && mArray[i] < 100) document.write( mArray[i]+ '  , ' )
-    //     if (mArray[i] == 100) document.write(mArray[i]+ '   ,' )
-    //     if (mArray[i] > 100) document.write( mArray[i] + ' ,  ')
-    //     if ((i % 10) == 0 && i > 0) { document.write('<br>') }
-    //     if ((i == 100) ) { document.write('<br>') }
-    // }
-
-
 
     n = 1
 
-    for (let i = 1; i < counter+1; i++) {
-        for (let j = 1; j < counter+1; j++) {
-            if(mArray[i] == mArray[j]) {
-                cArray[n]=0;
+    for (let i = 1; i < counter + 1; i++) {
+        for (let j = 1; j < counter + 1; j++) {
+            if (mArray[i] == mArray[j]) {
+                cArray[n] = 0;
             }
-           
         }
         n++
     }
     n = 1
-    for (let i = 1; i < counter+1; i++) {
-        for (let j = 1; j < counter+1; j++) {
-            if(mArray[i] == mArray[j]) {
+    for (let i = 1; i < counter + 1; i++) {
+        for (let j = 1; j < counter + 1; j++) {
+            if (mArray[i] == mArray[j]) {
                 cArray[n]++;
                 console.log("hit ->")
             }
@@ -64,7 +49,6 @@ function multiplicationMatrixColour() {
             console.log("j: " + j)
         }
         n++
-
     }
 
     for (let i = 1; i < cArray.length; i++) {
@@ -74,6 +58,5 @@ function multiplicationMatrixColour() {
         if (cArray[i] > 100) document.write('[_' + cArray[i] + '_] ')
         if ((i % 10) == 0 && i > 0) { document.write('<br>') }
     }
-
     return cArray
 }
