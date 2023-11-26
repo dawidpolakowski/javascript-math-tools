950004a2ab00c9c075b0c382ddfbcf07660108ca
Dawid Polakowski <dawidpolakowski@gmail.com>
2023-11-26 22:27:26 +0000

    add colors to matrix

--- multiplication/multiplicationcolour.js
+++ multiplication/multiplicationcolour.js
@@ -1,9 +1,9 @@
-
+let cArray = new Array(100)
 function multiplicationMatrixColour() {
 
     let counter = 100;
-    let mArray = new Array(counter)
-    let cArray = new Array(counter)
+    let mArray = new Array(100)
+   
 
     let n = 1
 
@@ -19,14 +19,6 @@ function multiplicationMatrixColour() {
         }
     }
 
-    for (let i = 1; i < mArray.length; i++) {
-        if (mArray[i] < 10) document.write('[__' + mArray[i] + '__] ')
-        if (mArray[i] >= 10 && mArray[i] < 100) document.write('[__' + mArray[i] + '_] ')
-        if (mArray[i] == 100) document.write('[_' + mArray[i] + '_] ')
-        if (mArray[i] > 100) document.write('[_' + mArray[i] + '_] ')
-        if ((i % 10) == 0 && i > 0) { document.write('<br>') }
-        if ((i == 100)) { document.write('<br>') }
-    }
 
     n = 1
 
@@ -57,6 +49,35 @@ function multiplicationMatrixColour() {
         if (cArray[i] == 100) document.write('[_' + cArray[i] + '_] ')
         if (cArray[i] > 100) document.write('[_' + cArray[i] + '_] ')
         if ((i % 10) == 0 && i > 0) { document.write('<br>') }
+        if ((i == 100)) { document.write('<br>') }
     }
+    for (let i = 1; i < mArray.length; i++) {
+        if (mArray[i] < 10) {
+            colorStyle(i) 
+            document.write('[__' + mArray[i] + '__] ' + "</span>")
+        }
+        if (mArray[i] >= 10 && mArray[i] < 100) {
+            colorStyle(i)
+            document.write('[__' + mArray[i] + '_] ' + "</span>")
+        } 
+        if (mArray[i] == 100){
+            colorStyle(i)
+            document.write('[_' + mArray[i] + '_] ' + "</span>")
+        }
+         
+        if (mArray[i] > 100){
+            colorStyle(i)
+            document.write('[_' + mArray[i] + '_] ' + "</span>")
+        } 
+        if ((i % 10) == 0 && i > 0) { document.write('<br>') }
+        
+    }
+
     return cArray
+}
+function colorStyle(i) {
+    if(cArray[i]==1) document.write('<span style="color:red">')
+    if(cArray[i]==2) document.write('<span style="color:orange">')
+    if(cArray[i]==3) document.write('<span style="color:blue">')
+    if(cArray[i]==4) document.write('<span style="color:green">')    
 }
